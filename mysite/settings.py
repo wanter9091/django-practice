64b653cc93b2ca410415eeb4a475e0565dc8332e
obj <dhqudwo2976@naver.com>
2022-08-03 01:43:21 +0900

    #5 ~09:05 마지막에 >>> response.context['latest_question_list'] Traceback (most recent call last):   File "<console>", line 1, in <module> TypeError: 'NoneType' object is not subscriptable 이렇게 제대로 안나옴;;

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c@teecrr-*&b@^kjsyd@nliimtwui#r_u7(ho(o)a73ccrq5p9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['testserver']
 
 
 # Application definition
