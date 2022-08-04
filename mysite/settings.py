1c4611a9f6db2a5e446dfce0fb4de259e33880fe
obj <dhqudwo2976@naver.com>
2022-08-05 01:33:52 +0900

    #6 end django에서의 static css 사용법

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c@teecrr-*&b@^kjsyd@nliimtwui#r_u7(ho(o)a73ccrq5p9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['testserver']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
 
 
 # Application definition
