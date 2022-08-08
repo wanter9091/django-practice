1379bd5280e9c529ca68415bdecff823e13ea8d2
obj <dhqudwo2976@naver.com>
2022-08-09 03:16:10 +0900

    #7 ~06:23 admin창에서 자잘한 기능 추가 방법

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):#인자로 admin.StackedInline도 가능
+    model= Choice
+    extra=3
+class QuestionAdmin(admin.ModelAdmin):#이걸 해주면 내가 원하는 순서대로 정렬가능
+    list_display=('question_text', 'pub_date', 'was_published_recently')#명시된 속성 출력(만약 잘못된 속성명 입력시 오류남)
+    fieldsets=[#내부 데이터 출력
+        (None,                  {'fields' : ['question_text']}),
+        ('Date information',    {'fields' : ['pub_date']}),
+    ]
+
+    inlines=[ChoiceInline]#이 부분으로 Choice의 값도 변경 가능
+
+    list_filter=['pub_date']#해당 속성에 대한 빠른 검색 선택지 추가
+    search_fields=['question_text']#해당 속성에 대한 검색창 추가
+
+
+
+admin.site.register(Question, QuestionAdmin)#이렇게 레지스터 함수를 사용해야 데이터를 어드민 페이지에서 데이터 수정 가능
+
 # Register your models here.
--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,10 @@ class Question(models.Model):
     def was_published_recently(self):
         now= timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    was_published_recently.admin_order_field='pub_date'#정렬 버튼을 눌렀을 때 정렬의 기준이 되는 속성 정하기
+    was_published_recently.boolean=True#이 부분이 트루면 was_published_recently의 bool값이 true같은 문자형이 아닌 그림으로 표시됨
+    was_published_recently.short_description='Published recently?'#출력되는 속성명을 변경
 
 class Choice(models.Model):
     question=models.ForeignKey(Question, on_delete=models.CASCADE)
